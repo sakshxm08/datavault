69bdd222272df595c99df65ddeb0fc15708b69f5
Saksham Gambhir <100534890+sakshxm08@users.noreply.github.com>
2023-07-23 23:51:01 +0530

    Update Cloud.js

--- src/pages/Cloud.js
+++ src/pages/Cloud.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Sidebar } from "../components/Sidebar";
 import { MainPage } from "../components/MainPage";
-import { useNavigate } from "react-router-dom";
+
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 export const Cloud = () => {
-  const [user, loader] = useAuthState(auth);
-  const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+
   return (
     <>
       {user ? (
